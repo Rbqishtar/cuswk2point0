cd3438dd2457e0990018e0d8fb62b082cd976681
rbqishtar <Shigurelover@hotmail.com>
2022-05-25 20:44:23 +0800

    test

---
+++ src/test/java/uicontrol/SeatCtrlTest.java
@@ -0,0 +1,48 @@
+package uicontrol;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeatCtrlTest {
+
+    @Test
+    void getSeat() {
+    }
+
+    @Test
+    void canInsertSeat() {
+    }
+
+    @Test
+    void insertSeat() {
+    }
+
+    @Test
+    void convertSeatNo() {
+    }
+
+    @Test
+    void reConvertSeatNo() {
+    }
+
+    @Test
+    void generateRandomSeat() {
+    }
+
+    @Test
+    void getRowFromSelectedIndex() {
+    }
+
+    @Test
+    void getMaxRowNumber() {
+    }
+
+    @Test
+    void getColNumber() {
+    }
+
+    @Test
+    void calculateSeatExtra() {
+    }
+}
