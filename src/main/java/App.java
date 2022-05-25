d2e3fc6234e3da556c937cc8c0fe2d0cdb080dc3
rbqishtar <Shigurelover@hotmail.com>
2022-05-26 00:21:49 +0800

    test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import ui.Welcome_0;
 
 public class App {
-    // I want to die
+    // FUCK GFW
     public static void main(String[] args) {
         System.out.println();
         Welcome_0 start = new Welcome_0();
