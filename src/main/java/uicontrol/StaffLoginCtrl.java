641159e318e4190556af32195866674324d730a5
rbqishtar <Shigurelover@hotmail.com>
2022-05-25 18:12:07 +0800

    Revert "test"
    This reverts commit d30a09a0de608becbe8e470e5dc93fe2a312ea4f.

--- src/main/java/uicontrol/StaffLoginCtrl.java
+++
@@ -1,9 +0,0 @@
-package uicontrol;
-
-import entitydao.StaffDAO;
-
-public class StaffLoginCtrl {
-    public boolean validateStaff(String userName, String password) {
-        return new StaffDAO().validateStaff(userName, password);
-    }
-}
