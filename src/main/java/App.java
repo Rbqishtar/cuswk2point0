fb60df648553cacea009625cfd84b6586e6c8d93
rbqishtar <Shigurelover@hotmail.com>
2022-05-27 12:42:44 +0800

    added dependency for fancy ui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import ui.Welcome_0;
 
 public class App {
     // This fucking network is terrible !!!!!!!!!!
     public static void main(String[] args) {
+        FlatIntelliJLaf.setup();
         System.out.println();
         Welcome_0 start = new Welcome_0();
         start.setVisible(true);
