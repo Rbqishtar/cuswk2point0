17ebcbae07b92b5f6a3fd09cc6a3fabfc6ffd08d
rbqishtar <Shigurelover@hotmail.com>
2022-05-28 16:55:51 +0800

    Changed ui skin

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,11 @@
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import ui.Welcome_0;
 
 public class App {
 
     public static void main(String[] args) {
-        FlatIntelliJLaf.setup();
-        System.out.println();
+        FlatDarkLaf.setup();
         Welcome_0 start = new Welcome_0();
         start.setVisible(true);
     }
